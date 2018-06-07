cbf8d5004752086ecf908c23c0cf7f037d24acdb
Luciano Di Palma <lucisdp@gmail.com>
2018-06-07 12:58:51 +0200

    added documentation to ExplorationMEtrics and Metrics classes

--- src/main/java/explore/ExplorationMetrics.java
+++ src/main/java/explore/ExplorationMetrics.java
@@ -7,16 +7,33 @@ import java.util.*;
  * and normalizing values by a constant.
  */
 public class ExplorationMetrics {
+    /**
+     * List of metrics retrieved at each iteration of the exploration process
+     */
     private List<Metrics> metrics;
 
+    /**
+     * Initializes an empty list of metrics
+     */
     public ExplorationMetrics(){
         this.metrics = new ArrayList<>();
     }
 
-    public void add(Metrics map){
-        metrics.add(map);
+    /**
+     * Append a new metric to the end of the list.
+     * @param metric: metrics to be appended
+     */
+    public void add(Metrics metric){
+        metrics.add(metric);
     }
 
+    /**
+     * Static method performing the sum of two ExplorationMetric objects.
+     * @param metrics1: first argument to sum
+     * @param metrics2: second argument to sum
+     * @return a new ExplorationMetric object containing the sum of corresponding metrics at every position
+     * @throws IllegalArgumentException if objects have a different number of elements
+     */
     public static ExplorationMetrics sum(ExplorationMetrics metrics1, ExplorationMetrics metrics2){
         ExplorationMetrics result = new ExplorationMetrics();
         Iterator<Metrics> it1 = metrics1.iterator();
@@ -26,6 +43,7 @@ public class ExplorationMetrics {
             result.add(Metrics.sum(it1.next(), it2.next()));
         }
 
+        // if lists have different lengths, one of the iterators will still have elements remaining
         if (it1.hasNext() || it2.hasNext()){
             throw new IllegalArgumentException("Lists should have the same number of elements.");
         }
@@ -33,10 +51,22 @@ public class ExplorationMetrics {
         return result;
     }
 
+    /**
+     * Instance method for summing another ExplorationMetric to the current metric object. Equivalent to sum(this, metrics).
+     * @param metrics: object to sum
+     * @return a new ExplorationMetric object containing the sum of metrics
+     * @throws IllegalArgumentException if objects have a different number of elements
+     */
     public ExplorationMetrics sum(ExplorationMetrics metrics){
         return sum(this, metrics);
     }
 
+    /**
+     * Static method dividing all metrics in an ExplorationMetric object by a given number.
+     * @param metrics: object to be divided
+     * @param denominator: number to divide each metric by
+     * @return a new ExplorationMetric object containing the divided metrics at every position
+     */
     public static ExplorationMetrics divideByNumber(ExplorationMetrics metrics, int denominator){
         ExplorationMetrics result = new ExplorationMetrics();
 
@@ -47,10 +77,18 @@ public class ExplorationMetrics {
         return result;
     }
 
+    /**
+     * Instance method for dividing the current metric object by a number. Equivalent to divideByNumber(this, denominator).
+     * @param denominator: number to divide each metric by
+     * @return a new ExplorationMetric object containing the divided metrics at every position
+     */
     public ExplorationMetrics divideByNumber(int denominator){
         return divideByNumber(this, denominator);
     }
 
+    /**
+     * @return an iterator to the Metrics contained in this object. Elements are retrieved in the same order of insertion.
+     */
     public Iterator<Metrics> iterator(){
         return metrics.iterator();
     }
--- src/main/java/explore/Metrics.java
+++ src/main/java/explore/Metrics.java
@@ -4,13 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * This class is a simple storage of (String, Double) key-value pairs, where the String represents the metric's name and
+ * the Double the metric's value. We provide Map-like methods for adding new metrics and retrieving keys, and also helper
+ * methods for summing two metric objects and dividing their values by a given number.
+ */
 public class Metrics {
+    /**
+     * Collection of metrics to be stored
+     */
     private Map<String, Double> metrics;
 
+    /**
+     * Initialize an empty metric collection
+     */
     public Metrics() {
         metrics = new HashMap<>();
     }
 
+    /**
+     * @param name: name of the metric to be retrieved
+     * @return value of the metric with specified name.
+     * @throws RuntimeException if there isn't a metric of given name.
+     */
     public Double get(String name){
         Double value = metrics.get(name);
         if (value == null){
@@ -19,6 +35,12 @@ public class Metrics {
         return value;
     }
 
+    /**
+     * Adds a new metric to our mapping
+     * @param name: name of the new metric
+     * @param value: value of the new metric
+     * @throws RuntimeException if it already contains the given metric
+     */
     public void add(String name, Double value){
         if (metrics.containsKey(name)){
             throw new RuntimeException("Trying to include the same metric twice.");
@@ -26,28 +48,61 @@ public class Metrics {
         metrics.put(name, value);
     }
 
+    /**
+     * Adds all metrics in 'metric' to the current object.
+     * @param metric: metrics to be appended to this object
+     * @throws RuntimeException if any of the metrics to be added is already contained in this object
+     */
     public void addAll(Metrics metric){
         for (String key : metric.names()){
             this.add(key, metric.get(key));
         }
     }
 
+    /**
+     * Returns the names of all metrics stored in the current object. We do not guarantee that the order is the same as
+     * that of insertion.
+     * @return set of names
+     */
     public Set<String> names(){
         return metrics.keySet();
     }
 
-    public static Metrics sum(Metrics map1, Metrics map2){
+    /**
+     * Static method for summing two metric objects.
+     * @param metrics1: left hand side of sum
+     * @param metrics2: right hand side of sum
+     * @return new object containing the sum of the corresponding metrics
+     * @throws RuntimeException if objects do not contain the same metric names
+     */
+    public static Metrics sum(Metrics metrics1, Metrics metrics2){
+        if ( !metrics1.names().equals(metrics2.names()) ){
+            throw new IllegalArgumentException("Objects to not contain the same metrics.");
+        }
+
         Metrics result = new Metrics();
-        for (String key : map1.names()){
-            result.add(key, map1.get(key) + map2.get(key));
+        for (String key : metrics1.names()){
+            result.add(key, metrics1.get(key) + metrics2.get(key));
         }
         return result;
     }
 
+    /**
+     * Instance method for summing another Metric object to this one. Equivalent to sum(this, metric).
+     * @param metric: metric to sum to this object
+     * @return a new object containing the sum.
+     * @throws RuntimeException if objects do not contain the same metric names
+     */
     public Metrics sum(Metrics metric){
         return sum(this, metric);
     }
 
+    /**
+     * Static method dividing all metric values in an Metric object by a given number.
+     * @param metric: object to be divided
+     * @param denominator: number to divide each metric by
+     * @return a new Metrics object containing the divided metrics at every position
+     */
     public static Metrics divideByNumber(Metrics metric, int denominator){
         if (denominator == 0){
             throw new IllegalArgumentException("Dividing by zero.");
@@ -60,6 +115,11 @@ public class Metrics {
         return result;
     }
 
+    /**
+     * Instance method for dividing each metric value by a number. Equivalent to divideByNumber(this, denominator).
+     * @param denominator: number to divide each metric by
+     * @return a new Metrics object containing the divided metrics
+     */
     public Metrics divideByNumber(int denominator){
         return divideByNumber(this, denominator);
     }
