9ede4e29ee7cfb9fc92b2580ec7cf65f544fb168
Luciano Di Palma <lucisdp@gmail.com>
2018-06-08 11:43:34 +0200

    documented UpperBoundCalculator class' '

--- src/main/java/learner/UpperBoundCalculator.java
+++ src/main/java/learner/UpperBoundCalculator.java
@@ -4,11 +4,34 @@ import classifier.BoundedClassifier;
 import classifier.BoundedLearner;
 import data.LabeledData;
 
+/**
+ * Utility module used for pruning the tree search in the ActiveTreeSearch algorithm.
+ */
 interface UpperBoundCalculator {
+    /**
+     * Compute the internal estimators
+     * @param data: labeled data
+     * @param steps: number of lookahead steps remaining
+     */
     void fit(LabeledData data, int steps);
+
+    /**
+     * Compute upper bound on maximum utility
+     * @param proba: probability of the given point being a target
+     * @return maximum utility upper bound
+     */
     double upperBound(double proba);
 }
 
+/**
+ * Tree Search optimization method described in [1]. The optimization consists of pruning the tree search by
+ * computing relatively inexpensive bound on the expected number of positive points to be retrieved.
+ *
+ * References:
+ *  [1]   Garnett, R., Krishnamurthy, Y., Xiong, X., Schneider, J.
+ *        Bayesian Optimal Active Search and Surveying
+ *        ICML, 2012
+ */
 class BoundedClassifierUpperBoundCalculator implements UpperBoundCalculator {
     private double u0, u1;
     private BoundedLearner learner;
@@ -25,7 +48,7 @@ class BoundedClassifierUpperBoundCalculator implements UpperBoundCalculator {
 
     /**
      * Helper function for computing the upper bound on the utility. It is also a recursive function, but it only needs to
-     * be computed once, and not for every point in the unlabeled set. Refer to [2] for details.
+     * be computed once, and not for every point in the unlabeled set. Refer to [1] for details.
      * @param steps: remaining steps to run
      * @param maxLabeledPoints: maximum number of positive points that can be added to current labeled set
      * @return upper bound on optimal utility
@@ -48,6 +71,9 @@ class BoundedClassifierUpperBoundCalculator implements UpperBoundCalculator {
     }
 }
 
+/**
+ * Dummy class used for general classifiers / learners. Basically, it lets the full tree search to be performed.
+ */
 class DummyUpperBoundCalculator implements UpperBoundCalculator{
 
     public void fit(LabeledData data, int steps){
