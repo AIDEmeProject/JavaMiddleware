aa581f68ab90a4b0ed28cb06bd4c5b282e5bd3e1
Luciano Di Palma <lucisdp@gmail.com>
2018-08-01 20:44:34 +0200

    removed exceptions package

--- src/main/java/exceptions/MetricAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class MetricAlreadyExistsException extends RuntimeException {
-    public MetricAlreadyExistsException(String name) {
-        super("Metric " + name + " already present in object.");
-    }
-}
--- src/main/java/exceptions/MetricNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class MetricNotFoundException extends RuntimeException {
-    public MetricNotFoundException(String name) {
-        super("Object does not contain the requested metric: " + name);
-    }
-}
