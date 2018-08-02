8834cd11af1adf3b5b5d4fb48db05b08afe27f02
Luciano Di Palma <lucisdp@gmail.com>
2018-08-02 11:53:36 +0200

    added documentation to FolderManager class; new files have '.run' extension now; file is created on disk imediately after calling the method

--- src/main/java/io/FolderManager.java
+++ src/main/java/io/FolderManager.java
@@ -1,10 +1,22 @@
 package io;
 
 import java.io.File;
+import java.io.IOException;
 
+/**
+ * This class manages a folder in the local filesystem. It can create new files in the folder, and return a list of
+ *
+ */
 public class FolderManager {
+    /**
+     * File object pointing to folder
+     */
     private File folder;
 
+    /**
+     * @param path: path to folder
+     * @throws RuntimeException if folder fails to be created or if path is not a directory
+     */
     public FolderManager(String path) {
         folder = new File(path);
 
@@ -17,12 +29,27 @@ public class FolderManager {
         }
     }
 
+    /**
+     * @return a new file object
+     * @throws RuntimeException if file already exists or an IO error occurs
+     */
     public File createNewFile() {
         String path = folder.getAbsolutePath();
-        return new File(path + File.separator + "run_" + System.currentTimeMillis() + ".jsonl");
+        File newFile = new File(path + File.separator + System.currentTimeMillis() + ".run");
+        try {
+            if (!newFile.createNewFile()) {
+                throw new RuntimeException("File already exists!");
+            }
+        } catch (IOException ex){
+            throw new RuntimeException("Failed to create file: " + newFile.getName(), ex);
+        }
+        return newFile;
     }
 
+    /**
+     * @return list of run files in folder
+     */
     public File[] getRuns(){
-        return folder.listFiles(file -> file.getName().startsWith("run"));
+        return folder.listFiles(file -> file.getName().endsWith(".run"));
     }
 }
