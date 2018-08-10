b6211725e2f5b25faba4f36b627c016cca037283
Luciano Di Palma <lucisdp@gmail.com>
2018-08-11 01:17:30 +0200

    added documentation to Kernel interface and GaussianKernel class; added unit tests to GaussianKernel

--- src/main/java/classifier/kernel/GaussianKernel.java
+++ src/main/java/classifier/kernel/GaussianKernel.java
@@ -3,14 +3,31 @@ package classifier.kernel;
 import utils.Validator;
 import utils.linalg.LinearAlgebra;
 
+/**
+ * The gaussian kernel is defined as:
+ *
+ *    \( k(x,y) = \exp \left( -\gamma \Vert x - y \Vert^2 \right) \)
+ *
+ * where gamma is a positive number. In practice, one usually chooses gamma = 1.0 / num_features.
+ */
 public class GaussianKernel implements Kernel {
+    /**
+     * gamma parameter
+     */
     private double gamma;
 
+    /**
+     * @param gamma gamma parameter of gaussian kernel
+     * @throws IllegalArgumentException if gamma is not positive
+     */
     public GaussianKernel(double gamma) {
         Validator.assertPositive(gamma);
         this.gamma = gamma;
     }
 
+    /**
+     * Uses the default value of 1.0 / num_features for gamma
+     */
     public GaussianKernel() {
         this.gamma = 0;
     }
--- src/main/java/classifier/kernel/Kernel.java
+++ src/main/java/classifier/kernel/Kernel.java
@@ -4,15 +4,33 @@ import data.DataPoint;
 
 import java.util.Collection;
 
+/**
+ * A kernel is any function k(x,y) satisfying the Mercer conditions:
+ *
+ *      - Symmetry: k(x,y) = k(y,x)
+ *      - Positivity: for all \( \{x_1, \ldots, x_n\}, \) the matrix \(K_{ij} = k(x_i, x_j\) is positive definite
+ */
 public interface Kernel {
+    /**
+     * @param x: a vector
+     * @param y: a vector
+     * @return the kernel function applied on the input vectors: k(x,y)
+     */
     double compute(double[] x, double[] y);
 
+    /**
+     * @param x: a data point
+     * @param y: a data point
+     * @return the kernel function applied on the input data points: k(x,y)
+     */
     default double compute(DataPoint x, DataPoint y){
         return compute(x.getData(), y.getData());
     }
 
     /**
-     * Given y and a collection {x_1, ..., x_n}, computes (k(x_1, y), ..., k(x_n, y))
+     * @param xs a collection of data points
+     * @param y a data point
+     * @return computes the vector \([k(x_1, y), ..., k(x_n, y)]\)
      */
     default double[] compute(Collection<? extends DataPoint> xs, DataPoint y){
         double[] kernelVector = new double[xs.size()];
@@ -26,7 +44,8 @@ public interface Kernel {
     }
 
     /**
-     * Given {x_1, ..., x_n}, computes the kernel matrix K = [k(x_i, x_j)]
+     * @param xs a collection of data points
+     * @return the kernel matrix \(K_{ij} = k(x_i, x_j)\)
      */
     default double[][] compute(Collection<? extends DataPoint> xs){
         double[][] kernelMatrix = new double[xs.size()][xs.size()];
