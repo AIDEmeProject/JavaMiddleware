d641edcce1df553e07bf180a972d0f0991bdcd44
Luciano Di Palma <lucisdp@gmail.com>
2018-09-05 16:24:29 +0200

    Refactored FolderManager class Removed RunFileParser class

--- src/main/java/io/FolderManager.java
+++ src/main/java/io/FolderManager.java
@@ -1,67 +1,103 @@
 package io;
 
-import java.io.File;
-import java.io.IOException;
+import data.LabeledPoint;
+import explore.ExperimentConfiguration;
+import json.JsonConverter;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * This class manages a folder in the local filesystem. It can create new files in the folder, and return a list of
- *
  */
 public class FolderManager {
+    private static String CONFIG_FILE = "config.json";
+    private static String RUN_EXT = ".run";
+    private static String EVAL_EXT = ".eval";
+    private static String TEMP_EXT = ".tmp";
+
     /**
      * File object pointing to folder
      */
-    private File folder;
+    private final Path folder;
 
     /**
      * @param path: path to folder
      * @throws RuntimeException if folder fails to be created or if path is not a directory
      */
     public FolderManager(String path) {
-        folder = new File(path);
+        folder = Paths.get(path);
 
-        if(!folder.exists() && !folder.mkdirs()){
-           throw new RuntimeException("Failed to create output folder at: " + path);
-        }
-
-        if (!folder.isDirectory()){
+        if (!Files.isDirectory(folder)){
             throw new RuntimeException("Path " + path + " does not represent an existing directory.");
         }
     }
 
-    /**
-     * @return a new .run file
-     * @throws RuntimeException if file already exists or an IO error occurs
-     */
-    public File createNewRunFile() {
-        return createNewFile(System.currentTimeMillis() + ".run");
-    }
+    public int getNewRunFileIndex() {
+        File[] files = folder.toFile().listFiles(x -> x.getName().endsWith(".run"));
 
-    /**
-     * @return a new .average file
-     * @throws RuntimeException if file already exists or an IO error occurs
-     */
-    public File createNewOutputFile() {
-        return createNewFile(System.currentTimeMillis() + ".average");
-    }
-
-    private File createNewFile(String filename) {
-        String path = folder.getAbsolutePath();
-        File newFile = new File(path + File.separator + filename);
-        try {
-            if (!newFile.createNewFile()) {
-                throw new RuntimeException("File already exists!");
-            }
-        } catch (IOException ex){
-            throw new RuntimeException("Failed to create file: " + newFile.getName(), ex);
+        if (files == null || files.length == 0) {
+            return 1;
         }
-        return newFile;
+
+        Arrays.sort(files);
+        String name = files[files.length - 1].getName();
+        int index = Integer.parseInt(name.substring(0, name.length() - 4));
+        return index + 1;
     }
 
-    /**
-     * @return list of run files in folder
-     */
-    public File[] getRuns() {
-        return folder.listFiles(file -> file.getName().endsWith(".run"));
+    public Path getRunFile(int index) {
+        return getPathToFile(index + RUN_EXT);
+    }
+
+    public Path getEvalFile(int index) {
+        return getPathToFile(index + EVAL_EXT);
+    }
+
+    public Path getTempFile(int index) {
+        return getPathToFile(index + TEMP_EXT);
+    }
+
+    private Path getPathToFile(String filename) {
+        return folder.resolve(filename);
+    }
+
+    public ExperimentConfiguration parseConfigurationFile() {
+        ExperimentConfiguration configuration;
+        try {
+            Path config = getPathToFile(CONFIG_FILE);
+            Reader reader = new FileReader(config.toFile());
+            configuration = JsonConverter.deserialize(reader, ExperimentConfiguration.class);
+        }
+        catch (FileNotFoundException ex) {
+            throw new RuntimeException(CONFIG_FILE + " file not found on " + folder);
+        }
+
+        if (configuration == null) {
+            throw new RuntimeException("Empty config file on folder " + folder);
+        }
+
+        return configuration;
+    }
+
+    public List<List<LabeledPoint>> parseRunFile(int index) {
+        if (Files.notExists(getRunFile(index))) {
+            return Collections.emptyList();
+        }
+
+        try(BufferedReader reader = Files.newBufferedReader(getRunFile(index))) {
+            return reader.lines()
+                    .map(JsonConverter::deserializeLabeledPoints)
+                    .collect(Collectors.toList());
+        }
+        catch (IOException ex) {
+            throw new RuntimeException("Failed to parse run file.", ex);
+        }
     }
 }
