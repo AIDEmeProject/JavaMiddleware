8bdaf3cd12541971a41379f32d9846f79c7fdaa5
Luciano Di Palma <lucisdp@gmail.com>
2018-09-05 17:41:54 +0200

    Removed GeneralizedBinarySearch class, since it was a duplicate of UncertaintySampling
    Removed GBS from ActiveLearnerAdapter

--- src/main/java/json/ActiveLearnerAdapter.java
+++ src/main/java/json/ActiveLearnerAdapter.java
@@ -18,8 +18,6 @@ class ActiveLearnerAdapter extends JsonDeserializedAdapter<ActiveLearner> {
                 return "UncertaintySampler";
             case "SIMPLEMARGIN":
                 return "SimpleMargin";
-            case "GBS":
-                return "GeneralizedBinarySearch";
             default:
                 throw new UnknownClassIdentifierException("ActiveLearner", identifier);
         }
