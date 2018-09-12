38d96d74569f705e45bb504139338ec90255180b
Luciano Di Palma <lucisdp@gmail.com>
2018-09-12 18:07:16 +0200

    Improved documentation of run.py

--- run.py
+++ run.py
@@ -2,30 +2,45 @@ import subprocess
 
 from scripts import *
 
-######################################
-# CONFIGURATIONS
-######################################
 
-TASK = "sdss_Q1_0.1%"  # task id, as defined in the tasks.ini file
-SUBSAMPLE_SIZE = 50000  # size of random, unlabeled sample . Use float('inf') if no sub-sampling is to be performed
+#############################
+# EXPERIMENT CONFIGURATIONS
+#############################
+# task id, as defined in the tasks.ini file
+TASK = "sdss_Q1_0.1%"
 
-MODE = [  #
-    'NEW',  # run new exploration
-    # 'RESUME',  # resume a previous exploration
-    'EVAL',   # run evaluation procedure over finished runs
-    'AVERAGE'  # average all evaluation file for a given metric
+# size of random, unlabeled sample . Use float('inf') if no sub-sampling is to be performed
+SUBSAMPLE_SIZE = 50000
+
+# Run modes to perform. There are four kinds: NEW, RESUME, EVAL, and AVERAGE
+MODES = [
+    'NEW',       # run new exploration
+    # 'RESUME',    # resume a previous exploration
+    'EVAL',      # run evaluation procedure over finished runs
+    'AVERAGE'    # average all evaluation file for a given metric
 ]
-NUM_RUNS = 1  # Number of new explorations to run. Necessary for the NEW mode only
-BUDGET = 20   # Maximum number of labeled points to be labeled by the user. Necessary for NEW and RESUME modes
-RUNS = [2]    # Runs to perform evaluation. Necessary for RESUME and EVAL modes
-METRICS = [   # Evaluation metrics. Necessary for EVAL and AVERAGE modes
+
+# Number of new explorations to run. Necessary for the NEW mode only
+NUM_RUNS = 1
+
+# Maximum number of labeled points to be labeled by the user. Necessary for NEW and RESUME modes
+BUDGET = 20
+
+# Runs to perform evaluation. Necessary for RESUME and EVAL modes
+RUNS = [2]
+
+# Evaluation metrics. Necessary for EVAL and AVERAGE modes.
+# Check the scripts/metrics.py file for all possibilities
+METRICS = [
     ConfusionMatrix(SVM(C=1e7, kernel='gaussian')),
     TargetSetAccuracy()
 ]
 
-ACTIVE_LEARNER = SimpleMargin(C=1e7, kernel="gaussian", gamma=0)
-ACTIVE_LEARNER = RandomSampler()
-ACTIVE_LEARNER = ActiveTreeSearch(KNN(k=100, gamma=0.1), lookahead=1)
+# Active Learning algorithm to run. Necessary for RUN and RESUME modes.
+# Check the scripts/active_learners.py file for all possibilities
+# ACTIVE_LEARNER = SimpleMargin(C=1e7, kernel="gaussian", gamma=0)
+# ACTIVE_LEARNER = RandomSampler()
+# ACTIVE_LEARNER = ActiveTreeSearch(KNN(k=100, gamma=0.1), lookahead=1)
 ACTIVE_LEARNER = UncertaintySampler(MajorityVote(
     num_samples=8,
     warmup=100, thin=10, chain_length=64, selector="single", rounding=True, cache=True,  # hit-and-run
@@ -33,12 +48,13 @@ ACTIVE_LEARNER = UncertaintySampler(MajorityVote(
     add_intercept=True, solver="ojalgo")  # extra
 )
 
+
 #############################
 # DO NOT CHANGE
 #############################
 
 # PARAMETER VALIDATION
-for mode in MODE:
+for mode in MODES:
     assert_in_list(mode, ['NEW', 'RESUME', 'EVAL', 'AVERAGE'])
 assert_positive("NUM_RUNS", NUM_RUNS)
 assert_positive("BUDGET", BUDGET)
@@ -46,7 +62,6 @@ assert_positive("SUBSAMPLE_SIZE", SUBSAMPLE_SIZE)
 
 # BUILD EXPERIMENT
 experiment_dir = os.path.join('experiment', TASK, ACTIVE_LEARNER.name, str(ACTIVE_LEARNER))
-
 experiment = Experiment(task=TASK, subsample=SUBSAMPLE_SIZE, active_learner=ACTIVE_LEARNER)
 experiment.dump_to_config_file(experiment_dir)
 
@@ -54,7 +69,7 @@ experiment.dump_to_config_file(experiment_dir)
 command_line = [
     "java -cp target/data_exploration-1.0-SNAPSHOT-jar-with-dependencies.jar RunExperiment",
     "--experiment_dir", experiment_dir,
-    "--mode", ' '.join(MODE),
+    "--mode", ' '.join(MODES),
     "--num_runs", str(NUM_RUNS),
     "--budget", str(BUDGET),
 ]
@@ -63,7 +78,7 @@ if RUNS:
     command_line.append("--runs")
     command_line.append(' '.join(map(str, RUNS)))
 
-if METRICS and ('EVAL' in MODE or 'AVERAGE' in MODE):
+if METRICS and ('EVAL' in MODES or 'AVERAGE' in MODES):
     command_line.append("--metrics")
     for m in METRICS:
         assert_is_instance(m, Metric)
