7fa3830d07b570dc36307705df3468cabd3c41d8
Luciano Di Palma <lucisdp@gmail.com>
2018-09-12 23:19:43 +0200

    Added documentation to JsonConverter class Included a new exception: JsonDeserializationException

--- src/main/java/io/json/JsonConverter.java
+++ src/main/java/io/json/JsonConverter.java
@@ -2,9 +2,12 @@ package io.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import data.LabeledPoint;
+import exceptions.JsonDeserializationFailedException;
 import explore.metrics.MetricCalculator;
 import machinelearning.active.ActiveLearner;
 import machinelearning.active.learning.versionspace.VersionSpace;
@@ -20,6 +23,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This module is responsible for deserializing JSON strings into java objects, and, conversely, serializing general java
+ * objects into JSON strings. More specifically, this class is a wrapper of Google's <a href="https://github.com/google/gson">Gson library</a>,
+ * a very well know and widely adopted library for such purposes.
+ */
 public class JsonConverter {
     private static final Gson gson = buildGson();
 
@@ -41,25 +49,69 @@ public class JsonConverter {
         return builder.create();
     }
 
-    public static <T> T deserialize(String json, Class<T> type) {
-        return gson.fromJson(json, type);
+    /**
+     * @param json: JSON string representation of a class T object
+     * @param type: class type to be deserialized
+     * @param <T>: class to deserialize
+     * @return an object of the class T deserialized from the JSON string input
+     * @throws JsonDeserializationFailedException if the json string failed to be deserialized
+     */
+    public static <T> T deserialize(String json, Class<T> type) throws JsonDeserializationFailedException {
+        try {
+            return gson.fromJson(json, type);
+        } catch (JsonParseException ex) {
+            throw new JsonDeserializationFailedException(type);
+        }
     }
 
-    public static <T> T deserialize(Reader reader, Class type) {
-        return gson.fromJson(new JsonReader(reader), type);
+    /**
+     * @param reader: a {@link Reader} object
+     * @param type: class type to be deserialized
+     * @param <T>: class to deserialize
+     * @return an object of the class T deserialized from the string obtained from the reader
+     * @throws JsonDeserializationFailedException if the json string failed to be deserialized
+     */
+    public static <T> T deserialize(Reader reader, Class<T> type) throws JsonDeserializationFailedException {
+        try {
+            return gson.fromJson(new JsonReader(reader), type);
+        } catch (JsonSyntaxException ex) {
+            throw new JsonDeserializationFailedException(type);
+        }
     }
 
-    public static List<LabeledPoint> deserializeLabeledPoints(String json) {
+    /**
+     * @param json: a JSON string encoding a list of {@link LabeledPoint} objects.
+     * @return the deserialized list of labeled points
+     * @throws JsonDeserializationFailedException if the json string failed to be deserialized
+     */
+    public static List<LabeledPoint> deserializeLabeledPoints(String json) throws JsonDeserializationFailedException {
         Type listType = new TypeToken<ArrayList<LabeledPoint>>(){}.getType();
-        return gson.fromJson(json, listType);
+        try {
+            return gson.fromJson(json, listType);
+        } catch (JsonSyntaxException ex) {
+            throw new JsonDeserializationFailedException(listType);
+        }
     }
 
-    public static Map<String, Double> deserializeMetricsMap(String json) {
+    /**
+     * @param json: a JSON string on the format {key : value}, where "value" must be a number
+     * @return a map of (String, Double) entries interpreted
+     * @throws JsonDeserializationFailedException if the json string failed to be deserialized
+     */
+    public static Map<String, Double> deserializeMetricsMap(String json) throws JsonDeserializationFailedException {
         Type mapType = new TypeToken<HashMap<String,Double>>(){}.getType();
-        return gson.fromJson(json, mapType);
+        try {
+            return gson.fromJson(json, mapType);
+        } catch (JsonParseException ex) {
+            throw new JsonDeserializationFailedException(mapType);
+        }
     }
 
-    public static <T> String serialize(T value) {
+    /**
+     * @param value: an object to be serialized
+     * @return a String serializing the input object
+     */
+    public static String serialize(Object value) {
         return gson.toJson(value);
     }
 }
