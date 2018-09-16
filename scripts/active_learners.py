59919546e2b01251a3b1f35eef1a532543d42e64
Luciano Di Palma <lucisdp@gmail.com>
2018-09-16 15:20:20 +0200

    Corrected SimpleMargin's JSON config file.

--- scripts/active_learners.py
+++ scripts/active_learners.py
@@ -5,8 +5,10 @@ class ActiveLearner(Printable):
     pass
 
 
-class SimpleMargin(ActiveLearner, SVM):
-    pass
+class SimpleMargin(ActiveLearner):
+    def __init__(self, C=1.0, kernel='gaussian', gamma=0):
+        super().__init__()
+        self.svmLearner = SVM(C=C, kernel=kernel, gamma=gamma)
 
 
 class RandomSampler(ActiveLearner):
