a53b52074b339018663a104245145284ba69890a
Luciano Di Palma <lucisdp@gmail.com>
2018-09-16 19:11:24 +0200

    Refactored OptimumFinder class   - Removed unused methods and corresponding unit tests also   - Renamed class into LinearSearch   - Prefixed each method with "find"
    Simplified MarginRanker and ProbabilityRanker classes

---
+++ src/test/java/utils/LinearSearchTest.java
@@ -0,0 +1,65 @@
+package utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class LinearSearchTest {
+    private List<Double> elems;
+    private FakeFunction score;
+
+
+    private class FakeFunction implements Function<Double, Double>{
+        private Function<Double, Double> function;
+        private int counter;
+
+        public FakeFunction(Function<Double, Double> function) {
+            this.counter = 0;
+            this.function = function;
+        }
+
+        @Override
+        public Double apply(Double aDouble) {
+            counter++;
+            return function.apply(aDouble);
+        }
+
+        public int getCounter() {
+            return counter;
+        }
+    }
+
+    @BeforeEach
+    void setUp() {
+        elems = Arrays.asList(-3.,-1., 2., 1.5, -2.5);
+        score = new FakeFunction(x -> x*x);
+    }
+
+    @Test
+    void minimizer_emptyInputCollection_throwsException() {
+        assertThrows(IllegalArgumentException.class, () -> LinearSearch.findMinimizer(new ArrayList<Double>(), x -> x));
+    }
+
+    @Test
+    void maximizer_emptyInputCollection_throwsException() {
+        assertThrows(IllegalArgumentException.class, () -> LinearSearch.findMaximizer(new ArrayList<Double>(), x -> x));
+    }
+
+    @Test
+    void minimizer_quadraticScoreFunction_returnsCorrectMinimum() {
+        assertEquals( new Double(-1), LinearSearch.findMinimizer(elems, score));
+    }
+
+    @Test
+    void maximizer_quadraticScoreFunction_returnsCorrectMaximum() {
+        assertEquals(new Double(-3),  LinearSearch.findMaximizer(elems, score));
+    }
+}
