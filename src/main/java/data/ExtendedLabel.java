93983798d73fefaa7a4639fce549b4c170ba55e4
Luciano Di Palma <lucisdp@gmail.com>
2018-09-23 18:22:47 +0200

    Added documentation to ExtendedClassifier and ExtendedLabel.
    Added a new method for predicting DataPoint objects into ExtendedClassifier

--- src/main/java/data/ExtendedLabel.java
+++ src/main/java/data/ExtendedLabel.java
@@ -2,9 +2,22 @@ package data;
 
 import machinelearning.classifier.Label;
 
+/**
+ * This enum is basically an extension of {@link Label} for {@link machinelearning.threesetmetric.ExtendedClassifier}.
+ * Give a data point, a ExtendedClassifier can predict it to belong into three classes:
+ *
+ *  - POSITIVE: interesting to the user
+ *  - NEGATIVE: uninteresting to the user
+ *  - UNKNOWN: unsure about the true label
+ */
 public enum ExtendedLabel {
     POSITIVE, NEGATIVE, UNKNOWN;
 
+    /**
+     * Attempts to convert this ExtendedLabel into the corresponding {@link Label} object.
+     * @return a Label object of same name (i.e. POSITIVE/NEGATIVE is converted into POSITIVE/NEGATIVE)
+     * @throws IllegalArgumentException if {@code this} is UNKNOWN
+     */
     public Label toLabel() {
         switch (this) {
             case POSITIVE:
@@ -16,6 +29,10 @@ public enum ExtendedLabel {
         }
     }
 
+    /**
+     * @param label a {@link Label} used in Machine Learning classifiers
+     * @return an ExtendedLabel of same name (i.e. POSITIVE/NEGATIVE is converted into POSITIVE/NEGATIVE)
+     */
     public static ExtendedLabel fromLabel(Label label) {
         return label == Label.POSITIVE ? POSITIVE : NEGATIVE;
     }
--- src/main/java/machinelearning/threesetmetric/ExtendedClassifier.java
+++ src/main/java/machinelearning/threesetmetric/ExtendedClassifier.java
@@ -6,14 +6,42 @@ import machinelearning.classifier.Label;
 
 import java.util.Collection;
 
+/**
+ * A ExtendedClassifier is responsible for building an accurate model of the user interest and disinterest regions. This model
+ * can be continuously updated as the real user provides more feedback, and for any given point its label can be predicted.
+ *
+ * In contrast to the usual Machine Learning classifier, a ExtendedClassifier may return one of three possible labels:
+ * POSITIVE, NEGATIVE, or UNKNOWN. See {@link ExtendedLabel} for more details.
+ */
 public interface ExtendedClassifier {
+    /**
+     * Update the current data model with new labeled data.
+     * @param point: a data point
+     * @param label: the data point's label
+     */
     void update(double[] point, Label label);
 
+    /**
+     * @param point: a data point
+     * @return the predicted label for input point
+     */
     ExtendedLabel predict(double[] point);
 
+    /**
+     * @param dataPoint: a data point
+     * @return the predicted label for input point
+     */
+    default ExtendedLabel predict(DataPoint dataPoint) {
+        return predict(dataPoint.getData());
+    }
+
+    /**
+     * @param points: a collection of data point
+     * @return the predicted labels for each point in the input collection
+     */
     default ExtendedLabel[] predict(Collection<DataPoint> points) {
         return points.stream()
-                .map(x -> predict(x.getData()))
+                .map(this::predict)
                 .toArray(ExtendedLabel[]::new);
     }
 }
