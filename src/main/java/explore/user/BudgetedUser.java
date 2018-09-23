6697c7b380bb234d530143e8d156061e975902e7
Luciano Di Palma <lucisdp@gmail.com>
2018-09-23 15:46:19 +0200

    Documented and added unit tests to BudgetedUser

--- src/main/java/explore/user/BudgetedUser.java
+++ src/main/java/explore/user/BudgetedUser.java
@@ -6,11 +6,31 @@ import utils.Validator;
 
 import java.util.Objects;
 
+/**
+ * A BudgetedUser represents a user who can only label a limited number of data points. It is meant to be used for testing
+ * new algorithms, and not during a real scenario.
+ */
 public final class BudgetedUser implements User {
+    /**
+     * A {@link User} who performs the labeling
+     */
     private final User user;
+
+    /**
+     * Upper bound on the number of points to label
+     */
     private final int budget;
+
+
+    /**
+     * Number of labeled points so far
+     */
     private int numberOfLabeledPoints;
 
+    /**
+     * @throws IllegalArgumentException if budget is not positive
+     * @throws NullPointerException if user is null
+     */
     public BudgetedUser(User user, int budget) {
         Validator.assertPositive(budget);
         this.user = Objects.requireNonNull(user);
@@ -18,20 +38,33 @@ public final class BudgetedUser implements User {
         this.numberOfLabeledPoints = 0;
     }
 
+    /**
+     * @return number of labeled points so far
+     */
     public int getNumberOfLabeledPoints() {
         return numberOfLabeledPoints;
     }
 
+    /**
+     * @return true if the user is still willing to continue labeling, i.e. less the "budget" data points have been labeled so far.
+     */
     public boolean isWilling() {
         return numberOfLabeledPoints < budget;
     }
 
+    /**
+     * @param point: point to label
+     * @return the label of the requested data point
+     * @throws IllegalStateException if attempting to label a data point when the budget has already been met.
+     */
     @Override
     public Label getLabel(DataPoint point) {
         if (!isWilling()) {
             throw new IllegalStateException("Attempting to request label after budget was reached.");
         }
+
         numberOfLabeledPoints++;
+
         return user.getLabel(point);
     }
 }
