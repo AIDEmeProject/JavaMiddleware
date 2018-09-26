617ad8f197a5ef9a4fe6439c31532abe2f4527c8
Enhui HUANG <enhui@223-73.wifi-inria-saclay.saclay.inria.fr>
2018-09-26 15:43:58 +0200

    add ExtendedClassifier and Label

--- src/main/java/machinelearning/classifier/ExtendedClassifier.java
+++ src/main/java/machinelearning/classifier/ExtendedClassifier.java
@@ -1,7 +1,20 @@
 package machinelearning.classifier;
 
+/**
+ * Update and prediction of three-class classifiers
+ */
 public interface ExtendedClassifier {
+    /**
+     * Update TSM structure at each iteration
+     * @param point latest data points
+     * @param label three-class
+     */
     void update(double[] point, Label label);
 
+    /**
+     * Prediction of three-class classifier
+     * @param point
+     * @return {-1, 0, 1}
+     */
     ExtendedLabel predict(double[] point);
 }
--- src/main/java/machinelearning/classifier/ExtendedLabel.java
+++ src/main/java/machinelearning/classifier/ExtendedLabel.java
@@ -1,5 +1,57 @@
 package machinelearning.classifier;
 
+/**
+ * Three classes: positive, negative and unknown
+ */
 public enum ExtendedLabel {
-    POSITIVE, NEGATIVE, UNKNOWN;
+    POSITIVE(1), NEGATIVE(-1), UNKNOWN(0);
+
+    private int sign;
+
+    /**
+     * Initialize the class with integer
+     * @param sign {-1,0,1}
+     */
+    ExtendedLabel(int sign) {
+        this.sign = sign;
+    }
+
+    /**
+     * @return whether the label is POSITIVE
+     */
+    public boolean isPositive() {
+        return (sign == 1);
+    }
+
+    /**
+     * @return whether the label is NEGATIVE
+     */
+    public boolean isNegative() {
+        return (sign == -1);
+    }
+
+    /**
+     * @return whether the label is UNKNOWN
+     */
+    public boolean isUnkown(){
+        return (sign == 0);
+    }
+
+    /**
+     * @return 1 if POSITIVE, -1 if NEGATIVE, 0 if UNKNOWN
+     */
+    public int asSign(){
+        return sign;
+    }
+
+    @Override
+    public String toString() {
+        if(sign > 0){
+            return "POSITIVE";
+        }else if (sign < 0){
+            return "NEGATIVE";
+        }else {
+            return "UNKNOWN";
+        }
+    }
 }
