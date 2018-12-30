812a24327d1ac27d0595b84eb1452d9c1633bf9c
Laurent Cetinsoy <laurent.cetinsoy@gmail.com>
2018-12-30 20:10:23 +0100

    remove typo

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
-echo "Building java backend...\n"
+echo "Building java backend..."
 
 mvn clean compile assembly:single
 targetFile="data_exploration-1.0-SNAPSHOT-jar-with-dependencies.jar"
 cp target/$targetFile build
 
-echo "building javascript frontend\n"
+echo "building javascript frontend"
 cd src/frontend/gui
 yarn build
 cp -r build ../../../build/frontend
 
-echo "packaging the application and copying to the web application download folder\n"
+echo "packaging the application and copying to the web application download folder"
 cd ../../../build
-zip aideplus.zip Dockerfile $targetFile frontend
+zip aideplus.zip Dockerfile $targetFile frontend docker_run.sh run.sh
 
 cp aideplus.zip ../src/webplatform/bin
 
