9ab62fd6da78bddf79b3e697919256fb26ab49ff
Laurent Cetinsoy <laurent.cetinsoy@gmail.com>
2019-12-09 23:06:28 +0100

    cars_encoded.csv must be added to the zip

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ cp -r build/* ../../../build/static
 
 echo "packaging the application and copying to the web application download folder"
 cd ../../../build
-zip -r aideplus.zip model_behavior Dockerfile $targetFile frontend launch_all.sh docker_run.sh run.sh static
+zip -r aideplus.zip model_behavior Dockerfile $targetFile frontend launch_all.sh docker_run.sh run.sh static cars_encoded.csv
 
 cp aideplus.zip ../src/webplatform/bin
 
