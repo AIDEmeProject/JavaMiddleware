f658bff2fbd178672e0cad6330fe899dd1f8ff0b
Laurent Cetinsoy <laurent.cetinsoy@gmail.com>
2019-12-11 21:31:37 +0100

    better axis

--- src/frontend/gui/src/components/visualisation/ModelBehaviorPlotter.js
+++ src/frontend/gui/src/components/visualisation/ModelBehaviorPlotter.js
@@ -15,7 +15,7 @@ class ModelBehaviorPlotter{
     createPlot(svgId, scale){
 
         // set the dimensions and margins of the graph
-        var margin = {top: 40, right: 20, bottom: 50, left: 70},
+        var margin = {top: 40, right: 20, bottom: 50, left: 80},
         width = 600 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
@@ -70,7 +70,7 @@ class ModelBehaviorPlotter{
           // text label for the y axis
         this.yLabel = svg.append("text")
                 .attr("transform", "rotate(-90)")
-                .attr("y", 0 - margin.left + 20)
+                .attr("y", 0 - margin.left + 10)
                 .attr("x",0 - (height / 2))
                 .attr("dy", "1em")
                 .style("text-anchor", "middle")
