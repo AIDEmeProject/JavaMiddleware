b633491f4b8dd6200f58c9723e831dbba43bdede
Laurent Cetinsoy <laurent.cetinsoy@gmail.com>
2019-12-11 21:35:52 +0100

    wording

--- src/frontend/gui/src/components/Welcome.js
+++ src/frontend/gui/src/components/Welcome.js
@@ -13,7 +13,7 @@ class Welcome extends Component{
 
             <div className="center">
 
-                <h1>Aideme</h1>
+                <h1>AIDEme</h1>
 
                 <p>                
                     <img src={welcomeImg} width="600" />
