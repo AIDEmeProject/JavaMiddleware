e38d31ed6d8797af25aaa46c0575dadc97d6f924
Laurent Cetinsoy <laurent.cetinsoy@gmail.com>
2019-12-12 02:02:14 +0100

    good stats for live

--- src/frontend/gui/src/components/Trace/Trace.js
+++ src/frontend/gui/src/components/Trace/Trace.js
@@ -203,7 +203,8 @@ class QueryTrace extends Component{
 
                                         <LabelInfos
                                             iteration={this.state.iteration}
-                                            labeledPoints={this.state.allLabeledPoints}                                            
+                                            labeledPoints={this.state.allLabeledPoints}   
+                                            live={false}                                         
                                         />
 
 
--- src/frontend/gui/src/components/visualisation/LabelInfos.js
+++ src/frontend/gui/src/components/visualisation/LabelInfos.js
@@ -16,33 +16,45 @@ class LabelInfos extends Component{
         
         const iteration = this.props.iteration
         const labeledPoints = this.props.labeledPoints.filter((e, i) => { return i <= iteration})
+
+        var nNegativeSamples = labeledPoints.filter(e => e.label === 0).length
+        var nPositiveSamples = labeledPoints.filter(e => e.label === 1).length
+        var nLabeledPoints = labeledPoints.length
+
+        if (this.props.live){
+            nLabeledPoints += 2
+            nNegativeSamples += 1
+            nPositiveSamples += 1
+        }
         
-        const negativeSamples = labeledPoints.filter(e => e.label === 0)
-        const positiveSamples = labeledPoints.filter(e => e.label === 1)
         
         return (
 
             <div id="iteration-labels">
                 <div>
                     Labeled sample {
-                        labeledPoints.length + 2
+                        nLabeledPoints
                     }
                 </div>
 
                 <div>
                     Positive labels {
-                        positiveSamples.length + 1
+                        nPositiveSamples
                     }
                 </div>
 
                 <div>
                     Negative labels {
-                        negativeSamples.length + 1
+                        nNegativeSamples
                     }
                 </div>
             </div>
         )
     }
+}
+
+LabelInfos.defaultProps = {
+    live:true
 }
 
 
