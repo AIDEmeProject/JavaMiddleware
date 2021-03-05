4f63a1d61b216445031cef8ea4fdc1dcee1661af
Vy Nguyen <vy.nguyen@mevry.com>
2021-03-05 12:45:20 +0100

    allow credentials on get-labeled-dataset request

--- src/frontend/gui/src/actions/getWholeLabeledDataset.js
+++ src/frontend/gui/src/actions/getWholeLabeledDataset.js
@@ -19,24 +19,25 @@
  */
 
 import $ from "jquery";
-import {backend, webplatformApi} from '../constants/constants'
+import { backend, webplatformApi } from "../constants/constants";
 
+function getWholedatasetLabeled() {
+  $.ajax({
+    type: "GET",
+    url: backend + "/get-labeled-dataset",
+    xhrFields: {
+      withCredentials: true,
+    },
+    success: (response) => {
+      var blob = new Blob([response]);
+      var link = document.createElement("a");
 
-function getWholedatasetLabeled(){
-
-    var url = backend + "/get-labeled-dataset"
-
-    $.get(url, response => {
-
-        var blob = new Blob([response]);
-        var link = document.createElement('a');
-        
-        link.href = window.URL.createObjectURL(blob);
-        document.body.appendChild(link);
-        link.download = "labeled_dataset.csv";
-        link.click();
-    })
-
+      link.href = window.URL.createObjectURL(blob);
+      document.body.appendChild(link);
+      link.download = "labeled_dataset.csv";
+      link.click();
+    },
+  });
 }
 
-export default getWholedatasetLabeled
+export default getWholedatasetLabeled;
